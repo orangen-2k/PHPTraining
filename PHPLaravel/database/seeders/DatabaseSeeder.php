1eeb365f59dd9703825a0120fec4c1658fe8e1a5
Nguyen Quyet Chien <orangen2000>
2021-01-23 17:11:11 +0700

    update

--- PHPLaravel/database/seeders/DatabaseSeeder.php
+++ PHPLaravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//        DB::table('users')->insert([
+//            'name'=>'Ph1u',
+//            'email'=>'phu@g1mail.com',
+//            'password'=>bcrypt('123')
+//        ]);
+        $this->call(usserSeeder::class);
+    }
+}
+
+class usserSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            ['name'=>'Pchw1q1su', 'email'=>'@g1m1awdcsaild.com', 'password'=>bcrypt('123q')],
+            ['name'=>'Pcchqwx1w1u', 'email'=>'@g1m1qwqassqail.com', 'password'=>bcrypt('12wc3')],
+//            ['name'=>'Pchccs1cw1u', 'email'=>str_random(300).'@gw1cmqcc1asail.com', 'password'=>bcrypt('12c3a')],
+        ]);
     }
 }
